67fcd401445b9b8667c5af71f1a931c6dfe49b30
Lucas <lgs.08lucas@gmail.com>
2024-06-01 00:20:26 -0300

    At 2024-06-01 Página de simulação de Flexbox Finalizada

--- assets/scripts/fexboxPageScript.js
+++ assets/scripts/fexboxPageScript.js
@@ -1,5 +1,9 @@
 const space = $('#testSpace');
-const select = $
+const selectDirection = $('#selectDirection')
+const selectJustify = $('#selectJustify')
+const selectAlign = $('#selectAlign')
+const selectWidth = $('#selectWidth')
+const selectHeight = $('#selectHeight')
 const itens = [];
 
 function attTestSpace() {
@@ -8,6 +12,8 @@ function attTestSpace() {
 
 function criarItem() {
     itens.push($('<div class="item">'+(itens.length+1)+'</div>'));
+    changeSize('width', selectWidth.val())
+    changeSize('height', selectHeight.val())
     attTestSpace()
 }
 
@@ -16,8 +22,70 @@ function removerItem() {
     attTestSpace()
 }
 
+function changeDirection() {
+    space.css('flex-direction', selectDirection.val())
+}
+
+function changeJustify() {
+    space.css('justify-content', selectJustify.val())
+}
+
+function changeAlign() {
+    space.css('align-items', selectAlign.val())
+}
+
+function changeSize(opt, e) {
+    switch (e) {
+        case "1":
+            itens.forEach(item => {
+                item.css(opt, '100%')
+            });
+            break;
+        case "2":
+            itens.forEach(item => {
+                item.css(opt, '100px')
+            });
+            break;
+        case "3":
+            itens.forEach(item => {
+                item.css(opt, item.text()*75+'px')
+            });
+            break;
+        default:
+            break;
+    }
+    space.css('align-items', selectAlign.val())
+}
+
+
+
+
+
+
+selectDirection.on('change', changeDirection)
+selectJustify.on('change', changeJustify)
+selectAlign.on('change', changeAlign)
+selectWidth.on('change', o=>{
+    changeSize('width', selectWidth.val())
+})
+selectHeight.on('change', o=>{
+    changeSize('height', selectHeight.val())
+})
+
+
+
+
+
+
+
+
+
 {
+    changeSize('width', selectWidth.val())
+    changeSize('height', selectHeight.val())
+    changeAlign()
+    changeJustify()
+    changeDirection()
     criarItem()
     criarItem()
 }
-
